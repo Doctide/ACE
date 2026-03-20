ab3bb873cedf8a7c94a2244e92822af5b1d8ce00
pourman <apourman@protonmail.com>
2026-03-20 10:41:00 -0700

    feat: add bounties
    - The following server properties have been added
      * bounty_system_enabled
      * bounty_allow_all_locations
      * bounty_allow_logged_out
      * bounty_expiration_time
      * bounty_max_contracts
      * pk_bounty_timer
    - The following admin commands have been added
      * /getloc
      * /forcepk

---
+++ Source/ACE.Server/WorldObjects/BountyContract_Properties.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACE.Entity.Enum.Properties;
+
+namespace ACE.Server.WorldObjects
+{
+    partial class BountyContract
+    {
+        public int? BountyTargetGuid
+        {
+            get => GetProperty(PropertyInt.BountyTargetGuid);
+            set { if (value == null) RemoveProperty(PropertyInt.BountyTargetGuid); else SetProperty(PropertyInt.BountyTargetGuid, value.Value); }
+        }
+
+        public int? BountyCreationTimeStamp
+        {
+            get => GetProperty(PropertyInt.BountyCreationTimestamp);
+            set { if (!value.HasValue) RemoveProperty(PropertyInt.BountyCreationTimestamp); else SetProperty(PropertyInt.BountyCreationTimestamp, value.Value); }
+        }
+
+        public bool BountyCompleted
+        {
+            get => GetProperty(PropertyBool.BountyCompleted) ?? false;
+            set { if (!value) RemoveProperty(PropertyBool.BountyCompleted); else SetProperty(PropertyBool.BountyCompleted, value); }
+        }
+    }
+}
